7c17af4e0ca16d672cf94c32dd3dd682ac9a3df2
konikanti vishnuvardhan Reddy <konikantivishnu424@gmail.com>
2025-03-10 15:16:44 +0530

    code Modifactions in Random Number

--- programing_class/src/simpleProgram/RandomNumber.java
+++ programing_class/src/simpleProgram/RandomNumber.java
@@ -7,7 +7,7 @@ public class RandomNumber {
 		System.out.println("1st Random Number: " + Math.random());   
 		System.out.println("2nd Random Number: " + Math.random());  
 		System.out.println("3rd Random Number: " + Math.random());   
-		System.out.println("4th Random Number: " + Math.random());   
+		   
 		}   
 
 	}
