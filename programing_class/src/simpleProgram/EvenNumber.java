ebf1407e7b2fe374acb8de7159d36b5795721b4f
konikanti vishnuvardhan Reddy <konikantivishnu424@gmail.com>
2025-03-10 14:55:53 +0530

    Even Number

---
+++ programing_class/src/simpleProgram/EvenNumber.java
@@ -0,0 +1,18 @@
+package simpleProgram;
+
+public class EvenNumber {
+
+	public static void main(String[] args) {
+		
+		
+		int num =10;
+		if(num%2==0) {
+			System.out.println("even number");
+		}else {
+			System.out.println("not a even number");
+		}
+		
+
+	}
+
+}
