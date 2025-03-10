411cea071521725548968e061542317776856129
konikanti vishnuvardhan Reddy <konikantivishnu424@gmail.com>
2025-03-10 15:01:45 +0530

    odd Number programm

---
+++ programing_class/src/simpleProgram/OddNumber.java
@@ -0,0 +1,16 @@
+package simpleProgram;
+
+public class OddNumber {
+
+	public static void main(String[] args) {
+		
+		int num=7;
+		if(num%2==1) {
+			System.out.println("odd Number");
+		}else {
+			System.out.println("Not odd Number");
+		}
+
+	}
+
+}
